6c18da8048053cc02ddcef05246d3dbcf07aaa64
Kelso <deathmatrix@gmail.com>
2013-02-05 19:19:39 -0500

    Adding NetIonicApplet .java file so that we can start work

---
+++ Special Topics/src/net_ionic_equations/NetIonicApplet.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net_ionic_equations;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author JRKelso
+ */
+public class NetIonicApplet {
+    
+    public static void main (String[] args){
+        JOptionPane.showMessageDialog(null, "Hello World");
+    }
+    
+}
